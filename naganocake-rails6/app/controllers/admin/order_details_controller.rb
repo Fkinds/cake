3b8e1c8a215b25f15f7285715e4cc2a8867897df
Fkinds <ec2-user@ip-172-31-19-44.ap-northeast-1.compute.internal>
2023-11-25 10:05:59 +0000

    add validate

--- naganocake-rails6/app/controllers/admin/order_details_controller.rb
+++ naganocake-rails6/app/controllers/admin/order_details_controller.rb
@@ -4,18 +4,21 @@ class Admin::OrderDetailsController < ApplicationController
     @order = @order_detail.order
     @order_details = @order.order_details
 
-    is_updated = true
+    #is_updated = true
     recive_making_status = params[:order_detail][:making_status]
     @order_detail.update(making_status: recive_making_status)
     if @order_detail.making_status == "製作中"
       @order.update(status: "製作中")
     end
-    @order_details.each do |order_detail|
-      if order_detail.making_status != "製作完了"
-        is_updated = false
-      end
-    end
-    if is_updated == true
+    #@order_details.each do |order_detail|
+    #  if order_detail.making_status != "製作完了"
+    #    is_updated = false
+    #  end
+    #end
+    #if is_updated == true
+    #  @order.update(status: "発送準備中")
+    #end
+    if @order_details.all?{|making_status| making_status == "製作完了"}
       @order.update(status: "発送準備中")
     end
     redirect_to admin_order_path(@order)
